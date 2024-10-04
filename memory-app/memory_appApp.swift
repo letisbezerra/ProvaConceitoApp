c8ef724f8c5f62dfefed0161b886e4dcc622c1a7
Beatriz <beatrizfpeixoto@gmail.com>
2024-10-04 08:27:08 -0300

    Initial Commit

---
+++ memory-app/memory_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  memory_appApp.swift
+//  memory-app
+//
+//  Created by Beatriz Peixoto on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct memory_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
